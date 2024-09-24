41c4499da7f378839c17f1d7ac8cc85346009d32
Maurits van der Schee <maurits@vdschee.nl>
2024-09-25 01:01:28 +0200

    update readme

--- writer.php
+++ writer.php
@@ -41,7 +41,7 @@ class WampObserver
 
   public static function logging(): bool
   {
-    if (!self::$socket) {
+    if (self::$socket === null) {
       self::$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) ?: null;
       socket_set_option(self::$socket, SOL_SOCKET, SO_SNDTIMEO, ['sec' => 0, 'usec' => 1]);
       self::$connected = false;
