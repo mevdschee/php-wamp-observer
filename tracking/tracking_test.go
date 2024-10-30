d04db82cb38165c240a12f181598460cb8249d61
Maurits van der Schee <maurits@vdschee.nl>
2024-10-30 10:52:10 +0100

    add timeout test

--- tracking/tracking_test.go
+++ tracking/tracking_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/mevdschee/php-observability/metrics"
 )
 
-// TestTrack tracks the time time spent and count of request response pairs with
+// TestTrack tracks the time spent and count of request response pairs with
 // the same messageId and checks that these are properly reported in the metrics.
 func TestTrack(t *testing.T) {
 	stats := metrics.New()
@@ -42,6 +42,42 @@ func TestTrack(t *testing.T) {
 	}
 	got := string(body)
 	want := "wamp_in_responses_seconds_count{message=\"Hello\"} 1\nwamp_in_responses_seconds_sum{message=\"Hello\"} 0.001"
+	if !strings.Contains(got, want) {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+// TestTrackTimeout tests the response timeout after a request with the same
+// messageId and checks that this timeout is properly reported in the metrics.
+func TestTrackTimeout(t *testing.T) {
+	stats := metrics.New()
+	track := New(stats, 1*time.Millisecond)
+	protocol := "wamp"
+	direction := "in"
+	request := "[2, \"123\", \"Hello\", {\"message\": \"Hello world!\"}]"
+	err := track.Track(protocol+"_"+direction, request)
+	if err != nil {
+		t.Errorf("error tracking request: %q", err.Error())
+	}
+	time.Sleep(10 * time.Millisecond)
+	response := "[3, \"123\", \"Hello\", {\"message\": \"Hello world!\"}]"
+	err = track.Track(protocol+"_"+direction, response)
+	if err != nil {
+		t.Errorf("error tracking response: %q", err.Error())
+	}
+	w := httptest.NewRecorder()
+	stats.Write(w)
+	resp := w.Result()
+	gz, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		t.Errorf("error reading gz: %q", err.Error())
+	}
+	body, err := io.ReadAll(gz)
+	if err != nil {
+		t.Errorf("error reading body: %q", err.Error())
+	}
+	got := string(body)
+	want := "wamp_in_timeouts_seconds_count{message=\"Hello\"} 1\nwamp_in_timeouts_seconds_sum{message=\"Hello\"} 0.001"
 	if !strings.Contains(got, want) {
 		t.Errorf("got %s, want %s", got, want)
 	}
