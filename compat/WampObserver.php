2c6b0bec23e416bd907e08ff5decddb1f331cd98
Maurits van der Schee <maurits@vdschee.nl>
2024-10-30 12:11:56 +0100

    split php

--- compat/WampObserver.php
+++ compat/WampObserver.php
@@ -5,15 +5,19 @@ class WampObserver
     public static $address = 'localhost';
     public static $port = 6666;
 
-    private static $socket = false;
+    private static $socket = null;
     private static $connected = false;
     private static $connectAt = 0;
 
     public static function logging()
     {
-        if (self::$socket === false) {
+        if (self::$socket === null) {
             self::$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            socket_set_option(self::$socket, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 0, 'usec' => 1));
+            if (!self::$socket) {
+                self::$socket = null;
+            } else {
+                socket_set_option(self::$socket, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 0, 'usec' => 1));
+            }
             self::$connected = false;
         }
         if (!self::$connected) {
@@ -31,7 +35,7 @@ class WampObserver
         if (self::logging()) {
             $line = json_encode(array($protocol, $direction, $message));
             if (!@socket_write(self::$socket, $line . "\n", strlen($line) + 1)) {
-                self::$socket = false;
+                self::$socket = null;
                 self::$connected = false;
             }
         }
